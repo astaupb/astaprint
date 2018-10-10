9813a00cd5f5214880d84dd8cbb363e9f38cae61
Gerrit Pape <papeg@mail.upb.de>
2018-10-10 15:56:45 +0200

    i need more space.. or better not

--- src/job/data.rs
+++ src/job/data.rs
@@ -111,37 +111,37 @@ impl JobData
     {
         let mut header: Vec<u8> = Vec::with_capacity(8096);
         header.append(&mut
-                  b"\x1b\x25\x2d\x31\x32\x33\x34\x35
-                    \x58\x40\x50\x4a\x4c\x20\x43\x4f
-                    \x4d\x4d\x45\x4e\x54\x20\x50\x4a
-                    \x4c\x2c\x4c\x49\x4e\x55\x58\x2c
+                  b"\x1b\x25\x2d\x31\x32\x33\x34\x35\
+                    \x58\x40\x50\x4a\x4c\x20\x43\x4f\
+                    \x4d\x4d\x45\x4e\x54\x20\x50\x4a\
+                    \x4c\x2c\x4c\x49\x4e\x55\x58\x2c\
                     \x50\x44\x46\r\n".to_vec(),
         );
 
         header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x4a\x4f\x42
-                    \x20\x4e\x41\x4d\x45\x3d\"{}\"\r
+                   "\x40\x50\x4a\x4c\x20\x4a\x4f\x42\
+                    \x20\x4e\x41\x4d\x45\x3d\"{}\"\r\
                     \n", &self.uid
             ).as_bytes().to_owned(),
         );
 
         header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x4a\x4f\x42\x4e\x41\x4d\x45
+                   "\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x4a\x4f\x42\x4e\x41\x4d\x45\
                     \x3d\"{}\"\r\n", &self.uid
             ).as_bytes().to_owned(),
         );
 
         header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x54\x52\x41\x43\x4b\x49\x44
+                   "\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x54\x52\x41\x43\x4b\x49\x44\
                     \x3d\"{}\"\r\n", &self.uid
             ).as_bytes().to_owned(),
         );
 
         header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x55\x53\x45\x52\x4e\x41\x4d
+                   "\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x55\x53\x45\x52\x4e\x41\x4d\
                     \x45\x3d\"{}\"\r\n", &self.user_id
             ).as_bytes().to_owned(),
         );
@@ -151,10 +151,10 @@ impl JobData
         let (date, time) = dt.split_at(10);
 
         header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x44\x41\x54\x45\x3d\"{}\"
-                    \r\n\x40\x50\x4a\x4c\x20\x53\x45
-                    \x54\x20\x54\x49\x4d\x45\x3d\"{}
+                   "\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x44\x41\x54\x45\x3d\"{}\"\
+                    \r\n\x40\x50\x4a\x4c\x20\x53\x45\
+                    \x54\x20\x54\x49\x4d\x45\x3d\"{}\
                     \"\r\n", date, time
             ).as_bytes().to_owned(),
         );
@@ -162,15 +162,15 @@ impl JobData
         match self.options.a3 {
             false => {
                 header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x50\x41\x50\x45\x52\x3d\x41
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x50\x41\x50\x45\x52\x3d\x41\
                     \x34\r\n".to_vec(),
                 );
             },
             true => {
                 header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x50\x41\x50\x45\x52\x3d\x41
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x50\x41\x50\x45\x52\x3d\x41\
                     \x33\r\n".to_vec(),
                 );
             },
@@ -179,28 +179,28 @@ impl JobData
         match self.options.duplex {
             0 => {
                 header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x44\x55\x50\x4c\x45\x58\x3d
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x44\x55\x50\x4c\x45\x58\x3d\
                     \x4f\x46\x46\r\n".to_vec(),
                 );
             },
             1 => {
                 header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x44\x55\x50\x4c\x45\x58\x3d
-                    \x4f\x4e\r\n\x40\x50\x4a\x4c\x20
-                    \x53\x45\x54\x20\x42\x49\x4e\x44
-                    \x49\x4e\x47\x3d\x4c\x4f\x4e\x47
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x44\x55\x50\x4c\x45\x58\x3d\
+                    \x4f\x4e\r\n\x40\x50\x4a\x4c\x20\
+                    \x53\x45\x54\x20\x42\x49\x4e\x44\
+                    \x49\x4e\x47\x3d\x4c\x4f\x4e\x47\
                     \x45\x44\x47\x45\r\n".to_vec(),
                 );
             },
             2 => {
                 header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x44\x55\x50\x4c\x45\x58\x3d
-                    \x4f\x4e\r\n\x40\x50\x4a\x4c\x20
-                    \x53\x45\x54\x20\x42\x49\x4e\x44
-                    \x49\x4e\x47\x3d\x53\x48\x4f\x52
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x44\x55\x50\x4c\x45\x58\x3d\
+                    \x4f\x4e\r\n\x40\x50\x4a\x4c\x20\
+                    \x53\x45\x54\x20\x42\x49\x4e\x44\
+                    \x49\x4e\x47\x3d\x53\x48\x4f\x52\
                     \x54\x45\x44\x47\x45\r\n".to_vec(),
                 );
             },
@@ -212,15 +212,15 @@ impl JobData
                 // WHAT THE FUCK RICOH
                 true => {
                     header.append(&mut format!(
-                    "\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x43\x4f\x50\x49\x45\x53\x3d
+                    "\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x43\x4f\x50\x49\x45\x53\x3d\
                     {}\r\n",
                         self.options.copies
                     ).as_bytes().to_owned(),);
                 },
                 false => {
                     header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x53\x45\x54
+                   "\x40\x50\x4a\x4c\x20\x53\x45\x54\
                     \x20\x51\x54\x59\x3d{}\r\n",
                         self.options.copies
                     ).as_bytes().to_owned(),);
@@ -232,16 +232,16 @@ impl JobData
             match self.options.a3 {
                 false => {
                     header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x46\x49\x54\x54\x4f\x50\x41
-                    \x47\x45\x53\x49\x5a\x45\x3d\x41
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x46\x49\x54\x54\x4f\x50\x41\
+                    \x47\x45\x53\x49\x5a\x45\x3d\x41\
                     \x34\r\n".to_vec(),);
                 },
                 true => {
                     header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x46\x49\x54\x54\x4f\x50\x41
-                    \x47\x45\x53\x49\x5a\x45\x3d\x41
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x46\x49\x54\x54\x4f\x50\x41\
+                    \x47\x45\x53\x49\x5a\x45\x3d\x41\
                     \x33\r\n".to_vec(),);
                 },
             }
@@ -251,12 +251,12 @@ impl JobData
             match self.options.nup {
                 2 => {
                     header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
                     \x20\x4e\x55\x50\x3d\x32\r\n".to_vec(),);
                 },
                 4 => {
                     header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
                     \x20\x4e\x55\x50\x3d\x34\r\n".to_vec(),);
                 },
                 _ => (),
@@ -264,34 +264,34 @@ impl JobData
             match self.options.nuppageorder {
                 0 => {
                     header.append(&mut
-                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20
-                    \x4e\x55\x50\x50\x41\x47\x45\x4f
-                    \x52\x44\x45\x52\x3d\x52\x49\x47
-                    \x48\x54\x54\x48\x45\x4e\x44\x4f
+                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20\
+                    \x4e\x55\x50\x50\x41\x47\x45\x4f\
+                    \x52\x44\x45\x52\x3d\x52\x49\x47\
+                    \x48\x54\x54\x48\x45\x4e\x44\x4f\
                     \x57\x4e\r\n".to_vec(),);
                 },
                 1 => {
                     header.append(&mut
-                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20
-                    \x4e\x55\x50\x50\x41\x47\x45\x4f
-                    \x52\x44\x45\x52\x3d\x44\x4f\x57
-                    \x4e\x54\x48\x45\x4e\x52\x49\x47
+                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20\
+                    \x4e\x55\x50\x50\x41\x47\x45\x4f\
+                    \x52\x44\x45\x52\x3d\x44\x4f\x57\
+                    \x4e\x54\x48\x45\x4e\x52\x49\x47\
                     \x48\x54\r\n".to_vec(),);
                 },
                 2 => {
                     header.append(&mut
-                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20
-                    \x4e\x55\x50\x50\x41\x47\x45\x4f
-                    \x52\x44\x45\x52\x3d\x4c\x45\x46
-                    \x54\x54\x48\x45\x4e\x44\x4f\x57
+                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20\
+                    \x4e\x55\x50\x50\x41\x47\x45\x4f\
+                    \x52\x44\x45\x52\x3d\x4c\x45\x46\
+                    \x54\x54\x48\x45\x4e\x44\x4f\x57\
                     \x4e\r\n".to_vec(),);
                 },
                 3 => {
                     header.append(&mut
-                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20
-                    \x4e\x55\x50\x50\x41\x47\x45\x4f
-                    \x52\x44\x45\x52\x3d\x44\x4f\x57
-                    \x4e\x54\x48\x45\x4e\x4c\x45\x46
+                  b"\x50\x4a\x4c\x20\x53\x45\x54\x20\
+                    \x4e\x55\x50\x50\x41\x47\x45\x4f\
+                    \x52\x44\x45\x52\x3d\x44\x4f\x57\
+                    \x4e\x54\x48\x45\x4e\x4c\x45\x46\
                     \x54\r\n".to_vec(),);
                 },
                 _ => (),
@@ -300,8 +300,8 @@ impl JobData
 
         if self.options.range != "" && page_range_is_valid(&self.options.range) {
             header.append(&mut format!(
-                   "\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x50\x52\x49\x4e\x54\x50\x41
+                   "\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x50\x52\x49\x4e\x54\x50\x41\
                     \x47\x45\x53\x3d\"{}\"\r\n",
                     self.options.range
                 ).as_bytes().to_owned(),
@@ -310,22 +310,22 @@ impl JobData
 
         // set defaults for sanity
         header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x41\x55\x54\x4f\x54\x52\x41
-                    \x59\x43\x48\x41\x4e\x47\x45\x20
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x41\x55\x54\x4f\x54\x52\x41\
+                    \x59\x43\x48\x41\x4e\x47\x45\x20\
                     \x4f\x4e\r\n".to_vec(),
         );
 
         header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54
-                    \x20\x4d\x45\x44\x49\x41\x54\x59
-                    \x50\x45\x3d\x50\x4c\x41\x49\x4e
-                    \x4e\x4f\x52\x45\x43\x59\x43\x4c
+                  b"\x40\x50\x4a\x4c\x20\x53\x45\x54\
+                    \x20\x4d\x45\x44\x49\x41\x54\x59\
+                    \x50\x45\x3d\x50\x4c\x41\x49\x4e\
+                    \x4e\x4f\x52\x45\x43\x59\x43\x4c\
                     \x45\x44\r\n".to_vec(),
         );
         header.append(&mut
-                  b"\x40\x50\x4a\x4c\x20\x45\x4e\x54
-                    \x45\x52\x20\x4c\x41\x4e\x47\x55
+                  b"\x40\x50\x4a\x4c\x20\x45\x4e\x54\
+                    \x45\x52\x20\x4c\x41\x4e\x47\x55\
                     \x41\x47\x45\x3d\x50\x44\x46\r\n"
                     .to_vec(),
         );
@@ -337,9 +337,9 @@ impl JobData
         header.append(&mut buf);
 
         header.append(&mut
-                  b"\x1b\x25\x2d\x31\x32\x33\x34\x35
-                    \x58\x40\x50\x4a\x4c\x20\x45\x4f
-                    \x4a\r\n\x1b\x25\x2d\x31\x32\x33
+                  b"\x1b\x25\x2d\x31\x32\x33\x34\x35\
+                    \x58\x40\x50\x4a\x4c\x20\x45\x4f\
+                    \x4a\r\n\x1b\x25\x2d\x31\x32\x33\
                     \x34\x35\x58".to_vec(),
         );
         header
